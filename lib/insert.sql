c0bce6743db34323d7110d2a520944383c969733
Kevin Breid <kevinbreid@yahoo.com>
2023-10-23 11:54:38 +0200

    Building SQL Database with Python

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'Brown', 'Friendly', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Brown', 'Rowdy', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'Brown', 'Friendly', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'M', 'Brown', 'Friendly', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'Brown', 'Friendly', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'Brown', 'Grumpy', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'Brown', 'Friendly', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (Null, 7, 'M', 'Brown', 'Friendly', True);
+
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (refer back to how to create a record that doesn't have one value)'
